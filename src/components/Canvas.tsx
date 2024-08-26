1eee4d6deadc0d4d0b2dfe12d00b723f63407788
Ido Kadosh <1234kadosh@gmail.com>
2024-08-26 12:48:12 +0300

    feat: add option to draw square on Canvas component

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,15 +1,19 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { forwardRef, useCallback, useEffect, useImperativeHandle, useRef, useState } from 'react';
 import useEventListener from '../hooks/useEventListener';
 
+export type DrawMode = 'rectangle' | 'freeDraw' | 'ellipse';
+
 interface PropTypes {
-	height?: number;
-	width?: number;
 	backgroundColor?: string;
+	drawMode: DrawMode;
 }
 
 const Canvas = forwardRef(({ backgroundColor = '#cccccc', drawMode }: PropTypes, ref) => {
 	const canvasRef = useRef<HTMLCanvasElement>(null);
 	const [isDrawing, setIsDrawing] = useState(false);
+	const [startPos, setStartPos] = useState<{ x: number; y: number } | null>(null);
+	const [savedImageData, setSavedImageData] = useState<ImageData | null>(null);
+
 	useImperativeHandle(ref, () => ({
 		clearCanvas: () => {
 			if (canvasRef.current) {
@@ -36,26 +40,52 @@ const Canvas = forwardRef(({ backgroundColor = '#cccccc', drawMode }: PropTypes,
 				const context = canvasRef.current.getContext('2d');
 				const startX = ev.clientX - rect.left;
 				const startY = ev.clientY - rect.top;
-				context?.moveTo(startX, startY);
+				setStartPos({ x: startX, y: startY });
+				if (!context) return;
+				context.moveTo(startX, startY);
+				if (context && drawMode === 'rectangle') {
+					const imageData = context.getImageData(0, 0, canvasRef.current.width, canvasRef.current.height);
+					setSavedImageData(imageData);
+				}
 			}
 		},
-		[canvasRef]
+		[canvasRef, drawMode]
 	);
 
 	const handleMouseMove = useCallback(
 		(ev: React.MouseEvent) => {
 			if (!isDrawing || !canvasRef.current) return;
+
 			const rect = canvasRef.current.getBoundingClientRect();
 			const context = canvasRef.current.getContext('2d');
 			const currentX = ev.clientX - rect.left;
 			const currentY = ev.clientY - rect.top;
+
+			const freeDraw = () => {
 				context?.lineTo(currentX, currentY);
 				context?.stroke();
+			};
 
-			if (!context) return;
+			const drawRectangle = () => {
+				if (drawMode === 'rectangle' && startPos && savedImageData) {
+					const width = currentX - startPos.x;
+					const height = currentY - startPos.y;
+
+					if (!context) return;
+
+					// Clear the canvas to not have multiple rectangles
+					context.putImageData(savedImageData, 0, 0);
+
+					context.strokeRect(startPos.x, startPos.y, width, height);
+				}
+			};
+
+			drawMode === 'freeDraw' && freeDraw();
+			drawMode === 'rectangle' && drawRectangle();
 		},
 		[isDrawing, canvasRef, drawMode, startPos, savedImageData]
 	);
+
 	const handleResize = useCallback(() => {
 		if (!canvasRef.current) return;
 		var rect = canvasRef.current.parentElement?.getBoundingClientRect();
