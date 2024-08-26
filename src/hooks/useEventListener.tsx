e70c3267840b60b7b73cb0b02b1ac82ead2b0200
Ido Kadosh <1234kadosh@gmail.com>
2024-08-26 12:12:54 +0300

    fix: change useEventListener to handle listeners on window/document

--- src/hooks/useEventListener.tsx
+++ src/hooks/useEventListener.tsx
@@ -4,25 +4,26 @@ const useEventListener = <T extends Event | React.UIEvent>({
 	eventName,
 	callback,
 	ref,
+	target,
 	capture = false,
 }: {
 	eventName: keyof HTMLElementEventMap | string;
 	callback: (ev: T) => unknown;
-	ref: RefObject<HTMLElement | Document>;
+	ref?: RefObject<HTMLElement | Document>;
+	target?: Window | Document;
 	capture?: boolean;
 }): void => {
 	useEffect(() => {
-		const refElement = ref && ref.current;
-		if (!refElement) return;
+		const eventTarget: HTMLElement | Document | Window | null | undefined = target || ref?.current;
+		if (!eventTarget) return;
 		const listenerOptions = { capture };
 
-		// Type assertion as EventListener for compatibility
 		const eventListener = callback as unknown as EventListener;
 
-		refElement.addEventListener(eventName, eventListener, listenerOptions);
+		eventTarget.addEventListener(eventName, eventListener, listenerOptions);
 
 		return () => {
-			refElement.removeEventListener(eventName, eventListener, listenerOptions);
+			eventTarget.removeEventListener(eventName, eventListener, listenerOptions);
 		};
 	}, [eventName, ref, callback, capture]);
 };
